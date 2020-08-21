8d49604b0d392d095a61c941bdfaaa49913928db
nandusri <kasu.nandu@gmail.com>
2020-08-21 15:01:58 +0530

    login page implementation

---
+++ src/components/AppNav.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="nav">
+    <router-link v-if="loggedIn" to="/">Dashboard</router-link>
+    <router-link v-if="!loggedIn" to="/login" class="button btn-secondary">Login</router-link>
+    <button v-else type="button" class="button btn-secondary" @click="logout">Logout</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['loggedIn'])
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#nav {
+  display: flex;
+  align-items: center;
+  min-height: 50px;
+  padding: 0.2em 1em;
+  background: #1f364d;
+}
+
+.nav-welcome {
+  margin-left: auto;
+  margin-right: 1rem;
+  color: #9cb3c9;
+}
+
+a,
+button {
+  font-weight: bold;
+  color: #9cb3c9;
+  margin: auto 0.8em auto 0.4em;
+  text-decoration: none;
+  border-top: 2px solid transparent;
+  border-bottom: 2px solid transparent;
+}
+a:hover {
+  color: #fff;
+}
+
+.router-link-exact-active {
+  color: white;
+  border-bottom: 2px solid #fff;
+}
+
+button,
+.button {
+  margin-left: auto;
+  text-decoration: none;
+  color: white;
+  border: none;
+}
+.button:hover {
+  color: white;
+}
+
+.logoutButton {
+  cursor: pointer;
+}
+
+.nav-welcome + button {
+  margin-left: 0;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <div class="columns">
+      <div class="column">
+        <img src="images/travel_mode.svg" alt="pair-programming" />
+      </div>
+      <div class="column">
+        <h1 class="title is-2 has-text-white has-text-centered">Login</h1>
+        <form class="auth-form" @submit.prevent="login">
+          <label for="username">Username</label>
+          <input
+            v-model="username"
+            type="username"
+            name="username"
+            class="input"
+            placeholder="johnSmith"
+          />
+
+          <label for="password">Password</label>
+          <input
+            v-model="password"
+            type="password"
+            name="password"
+            class="input"
+            placeholder="Password"
+          />
+
+          <button type="submit" name="button" class="button btn-primary">Login</button>
+
+          <p class="has-text-danger">{{ error }}</p>
+
+          <router-link to="/register">Don't have an account? Register.</router-link>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    login() {
+        axios.post(' http://127.0.0.1:8000/api/users/login/', {
+	                "username": this.username,
+	                "password": this.password
+                })
+        .then((response) => {
+          console.log('response',response);
+          this.$router.push({ name: 'dashboard' })
+        })
+        .catch(err => {
+          this.error = err.response.data.error
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
